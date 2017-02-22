33e066b66cc08142d50fab19cdd1f06c7a24fc80
Manuel <m.baltieri@sussex.ac.uk>
2017-02-22 22:54:37 +0000

    Testing whether, given an OU process, the best way to infer it is to track the mean or instantaneously follow the changes.
    Conclusions suggest the latter.
    
    It is important however to distinguish between a parametrisation of time scale (tau) vs. one of decay factors (alpha/beta), the first one simply says that faster inference systems are bounded to perform better than slower ones up to numerical errors, the second one implies that a specific decay constant will give the best results when the other one is fixed to a certain value.

---
+++ BivariateOU.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 14:51:55 2017
+
+Bi-variate OU process to check what the best strategy is for one process to
+track the other one (get the mean vs. follow instantaneously)
+
+Conclusions:
+- follow instantaneously is better than just track the mean
+
+@author: mb540
+"""
+
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+dt = .01
+T = 200
+iterations = int(T / dt)
+plt.close('all')
+
+variables = 2
+
+w = 1.
+
+alpha_min = .1
+alpha_max = 16.
+steps = 10
+
+alpha_range = np.arange(alpha_min, alpha_max, (alpha_max - alpha_min) / steps)
+
+beta = - 1.
+
+MSE = np.zeros((steps))
+
+for i in range(steps):
+    sigma_x = .1
+    sigma_y = .1
+    dW = np.random.randn(variables, iterations)
+    
+    X = np.zeros((variables, iterations))
+    C = np.array([[- alpha_range[i], w], [beta, .0]])
+    sigma = np.array([[sigma_x, .0], [.0, sigma_y]])
+    SIGMA = - np.dot(sigma.transpose(), sigma)
+    
+    OMEGA = sp.linalg.solve_lyapunov(C, SIGMA)
+    
+    MSE[i] = OMEGA[0, 0] - 2 * OMEGA[0, 1] + OMEGA[1, 1]
+
+plt.figure()
+plt.plot(alpha_range, MSE)
+
+for j in range(steps):
+    C = np.array([[- alpha_range[j], w], [beta, .0]])
+    X = np.zeros((variables, iterations))
+    for i in range(iterations - 1):
+        dX = np.dot(C, X[:, i]) + np.dot(sigma, dW[:, i]) / np.sqrt(dt)
+        X[:, i + 1] = X[:, i] + dt * dX
+    
+#    plt.figure()
+#    plt.plot(np.arange(0, iterations*dt, dt), X[1, :], 'b')
+#    plt.plot(np.arange(0, iterations*dt, dt), X[0, :], 'r')
+    MSE[j] = np.mean((X[1, :] - X[0, :]) ** 2)
+
+
+plt.figure()
+plt.plot(alpha_range, MSE)
+
+
+tau_min = .001
+tau_max = 5.
+steps = 10
+
+tau_range = np.arange(tau_min, tau_max, (tau_max - tau_min) / steps)
+
+
+for i in range(steps):
+    sigma_x = .1
+    sigma_y = .1
+    dW = np.random.randn(variables, iterations)
+    
+    X = np.zeros((variables, iterations))
+    C = np.array([[- 1., w] / tau_range[i], [beta, .0]])
+    sigma = np.array([[sigma_x, .0], [.0, sigma_y]])
+    SIGMA = - np.dot(sigma.transpose(), sigma)
+    
+    OMEGA = sp.linalg.solve_lyapunov(C, SIGMA)
+    
+    MSE[i] = OMEGA[0, 0] - 2 * OMEGA[0, 1] + OMEGA[1, 1]
+
+plt.figure()
+plt.plot(alpha_range, MSE)
+
+
+for j in range(steps):
+    C = np.array([[- 1., w] / tau_range[j], [beta, .0]])
+    X = np.zeros((variables, iterations))
+    for i in range(iterations - 1):
+        dX = np.dot(C, X[:, i]) + np.dot(sigma, dW[:, i]) / np.sqrt(dt)
+        X[:, i + 1] = X[:, i] + dt * dX
+    
+    plt.figure()
+    plt.plot(np.arange(0, iterations*dt, dt), X[1, :], 'b')
+    plt.plot(np.arange(0, iterations*dt, dt), X[0, :], 'r')
+    MSE[j] = np.mean((X[1, :] - X[0, :]) ** 2)
+
+
+plt.figure()
+plt.plot(alpha_range, MSE)
+
+
+
+
+
+
